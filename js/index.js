86110bbb6b4781bbf748c1ea479180c1493db663
Xavier Bergeron <xavbergeron@gmail.com>
2021-01-13 09:42:01 -0800

    oops I hadn't saved

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ if (hacker1.length > hacker2.length) {
   console.log(
     `The driver has the longest name, it has ${hacker1.length} characters`
   )
-} else if (hacker1.length < hacker2.length) {
+} else if (hacker2.length > hacker1.length) {
   console.log(
     `It seems that the navigator has the longest name, it has ${hacker1.length} characters`
   )
@@ -22,6 +22,26 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+let build = ""
+for (let i = 0; i < hacker1.length; i++) {
+  build += hacker1[i].toUpperCase()
+  if (i < hacker1.length - 1) build += " "
+}
+console.log(build)
+
+build = ""
+for (let i = hacker1.length - 1; i >= 0; i--) {
+  build += hacker1[i]
+}
+console.log(build)
+
+if (hacker1 < hacker2) {
+  console.log("The driver goes first")
+} else if (hacker2 < hacker1) {
+  console.log("YO, the navigator goes first")
+} else {
+  console.log("You both have the same name, flip a coin")
+}
 
 const lorem =
   "Ut dolore ad veniam voluptate aliqua minim pariatur pariatur veniam eiusmod. Proident est quis officia tempor voluptate id id. Anim ad fugiat ea officia deserunt laboris sint anim consectetur nulla tempor. Magna incididunt nulla tempor esse labore. Lorem non voluptate do sint et minim tempor voluptate."
@@ -35,9 +55,9 @@ for (let i = 1; i < lorem.length; i++)
   if (lorem[i - 1] == "e" && lorem[i] == "t") numEt++
 
 function isPalindrome(str) {
-  str = str.replace(/[^\w]/, "").toLowerCase()
+  str = str.replace(/[^\w]/g, "").toLowerCase()
   const end = str.length - 1
-  for (let i = 0; i < Math.floor(end / 2); i++)
+  for (let i = 0; i < Math.ceil(end / 2); i++)
     if (str[i] != str[end - i]) return false
 
   return true
